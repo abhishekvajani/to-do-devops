f625bbabf6dbf75f400bc0e84cbe3e2dd98bcc2e
abhishekvajani <abhi.vajani@yahoo.com>
2024-10-19 00:57:01 +0530

    jenkins's docker

--- terraform/main.tf
+++ terraform/main.tf
@@ -116,7 +116,7 @@ resource "aws_instance" "jenkins_server" {
       sudo docker run -d --name jenkins \
     -p 8080:8080 -p 50000:50000 \
     -v jenkins_home:/var/jenkins_home \
-    -v /var/run/docker.sock:/var/run/docker.sock \  # Mount Docker socket
+    -v /var/run/docker.sock:/var/run/docker.sock \
     jenkins/jenkins:lts
   EOF
 }
