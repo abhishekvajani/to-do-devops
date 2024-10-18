8fd32222aacd8af84fdbe9a66d3e99e541d59d6a
abhishekvajani <abhi.vajani@yahoo.com>
2024-10-19 00:52:14 +0530

    docker inside jenkins

--- terraform/main.tf
+++ terraform/main.tf
@@ -114,10 +114,10 @@ resource "aws_instance" "jenkins_server" {
 
       # Run Jenkins in a Docker container
       sudo docker run -d --name jenkins \
-        -p 8080:8080 -p 50000:50000 \
-        -v jenkins_home:/var/jenkins_home \
-        -v /var/run/docker.sock:/var/run/docker.sock \  # Mount Docker socket
-        jenkins/jenkins:lts
+    -p 8080:8080 -p 50000:50000 \
+    -v jenkins_home:/var/jenkins_home \
+    -v /var/run/docker.sock:/var/run/docker.sock \  # Mount Docker socket
+    jenkins/jenkins:lts
   EOF
 }
 
