7613184d8500d33bb2c0a599cdd889400c3c42a0
abhishekvajani <abhi.vajani@yahoo.com>
2024-10-18 22:45:37 +0530

    jenkins port correction

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,8 +55,8 @@ resource "aws_security_group" "jenkins_sg" {
   }
 
   ingress {
-    from_port   = 8081
-    to_port     = 8081
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allow Jenkins access
   }
